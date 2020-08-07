079dca4cf219079ebc5fa70708eae16a0f44c584
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-07 11:28:04 -0500

    'Connection made within the server'

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const pool = require('./db/db');
 
 const app = express();
 const { PORT } = process.env;
