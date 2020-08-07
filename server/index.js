0034143fbc159c0590d159bc4b6c0a1563bb5ed5
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-06 19:31:29 -0500

    'Serving client successfully'

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const { PORT } = process.env;
+const CLIENT_PATH = path.join(__dirname, '../client/dist');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static(CLIENT_PATH));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
