6960ec3365edcadecc9d3b3765d0be3b5b366b93
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-07 11:25:01 -0500

    'Connecting to mariadb'

---
+++ server/db/db.js
@@ -0,0 +1,9 @@
+const mariadb = require('mariadb');
+
+const pool = mariadb.createPool({ host: '127.0.0.1', user: 'root', connectionLimit: 5 });
+
+pool.getConnection()
+  .then(() => console.log('Connecting to database'))
+  .catch((err) => console.error('Database Error: ', err));
+
+module.exports = pool;
