bb71a24725c90a501a915dbdfda84c2104da675a
Evan Taft <evtaft22@gmail.com>
2020-08-09 17:54:33 -0500

    "App file updated to render sidebar open button"

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Sidebar from './Sidebar.jsx';
 import Choice from './Choice.jsx';
 
 function App() {
-   const [ hiya ] = useState('hiya');
-   return(
+   const [ user ] = useState(null);
+   const [ dog ] = useState(null);
+
+   const open = () => {
+    document.getElementById("mySidenav").style.width = "280px";
+  }
+
+   return (
       <div>
-         <Choice hiya={hiya}/>
+         <Sidebar user={user} dog={dog} />
+         <Router>
+            <div className='App'>
+               <button id='settings' onClick={open}>Menu</button>
+               <Choice />
+            </div>
+         </Router>
       </div>
    );
-}
+};
+
 export default App;
