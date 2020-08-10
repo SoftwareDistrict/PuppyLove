9d1359c9c8f4f017fce12e007e29c725df727016
Evan Taft <evtaft22@gmail.com>
2020-08-10 10:41:43 -0500

    "Blank component's for frontend routes"

---
+++ client/component/Chats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Chats() {
+  return (
+    <div>
+      <h2>Chats</h2>
+    </div>
+  );
+};
+
+export default Chats;
---
+++ client/component/Friends.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Friends() {
+  return (
+    <div>
+      <h2>Friends List</h2>
+    </div>
+  );
+};
+
+export default Friends;
