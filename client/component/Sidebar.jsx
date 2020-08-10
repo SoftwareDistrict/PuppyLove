a87bd2d9e16e4727ecb17bac1499c15dc8e1e010
Evan Taft <evtaft22@gmail.com>
2020-08-09 20:50:27 -0500

    "Sidebar Link's, setup, and animation complete"

--- client/component/Sidebar.jsx
+++ client/component/Sidebar.jsx
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import Choice from './Choice.jsx';
-import Logout from './Logout.jsx';
-import MyProfile from './MyProfile.jsx';
-import Chats from './Chats.jsx';
-import DogProfile from './DogProfile.jsx';
-import Friends from './Friends.jsx';
-import FavLocations from './FavLocations.jsx';
-import PopularLocations from './PopularLocations.jsx';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Sidebar({ user, dog }) {
 
   const close = () => {
     document.getElementById("mySidenav").style.width = "0px";
-  }
+  };
 
   return (
     <div id="mySidenav" className="navbar">
       <button id='settings' onClick={close}>Menu</button>
+      <h3>PuppyLove!</h3>
       {dog ?
         (
           <ul className="nav">
@@ -46,42 +39,8 @@ function Sidebar({ user, dog }) {
           <li id='loc'><Link to="/popular">Popular Locations</Link></li>
         </ul>
       )}
-      <Switch>
-        <Route exact={true} path="/" render={() => (<Choice user={user} />)} />
-        <Route exact path="/logout" render={() => (<Logout user={user} />)} />
-        <Route path="/myprofile" render={() => (<MyProfile user={user} />)} />
-        <Route exact path="/chats" render={() => (<Chats user={user} />)} />
-        <Route path="/dogprofile" render={() => (<DogProfile user={user} dog={dog} />)} />
-        <Route path="/friends" render={() => (<Friends user={user} dog={dog} />)} />
-        <Route path="/favs" render={() => (<FavLocations user={user} dog={dog} />)} />
-        <Route path="/popular" render={() => (<PopularLocations user={user} />)} />
-      </Switch>
     </div>
   );
 };
 
 export default Sidebar;
-
-// return (
-// <div className="navbar">
-//   <button id='settings-close'>Menu</button>
-  // <ul className="nav">
-  //   <li id='choice'><Link to="/">Home</Link></li>
-  //   <li id='pro'><Link to="/myprofile">My Profile</Link></li>
-  //   <li id='chats'><Link to="/chats">Chats</Link></li>
-  //   <li id='dog'><Link to="/dogprofile">Profile</Link></li>
-  //   <li id='friend'><Link to="/friends">Friends</Link></li>
-  //   <li id='fav'><Link to="/favs">Favorite Locations</Link></li>
-  //   <li id='loc'><Link to="/popular">Popular Locations</Link></li>
-  // </ul>
-//   <Switch>
-//     <Route exact={true} path="/" render={() => (<Choice user={user} />)} />
-//     <Route path="/myprofile" render={() => (<MyProfile user={user} />)} />
-//     <Route exact path="/chats" render={() => (<Chats user={user} />)} />
-    // <Route path="/dogprofile" render={() => (<DogProfile user={user} />)} />
-    // <Route path="/friends" render={() => (<Friends user={user} />)} />
-    // <Route path="/favs" render={() => (<FavLocations user={user} />)} />
-//     <Route path="/popular" render={() => (<PopularLocations user={user} />)} />
-//   </Switch>
-// </div>
-// );
