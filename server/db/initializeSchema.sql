de12d9b9593b1536c46bc68e8520d62a11093674
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-09 21:43:56 -0500

    'Updated file'

--- server/db/initializeSchema.sql
+++ server/db/initializeSchema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS puppy_love
+DROP DATABASE IF EXISTS puppy_love;
 
-CREATE DATABASE puppy_love
+CREATE DATABASE puppy_love;
 
 CREATE TABLE IF NOT EXISTS user(`
 	id INT NOT NULL AUTO_INCREMENT,
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS user(`
 	home_latitude DECIMAL NOT NULL,
 	home_longitude DECIMAL NOT NULL,
 	PRIMARY KEY(id)
-`)
+`);
 
 CREATE TABLE IF NOT EXISTS dog(`
 	id INT NOT NULL AUTO_INCREMENT,
@@ -26,7 +26,7 @@ CREATE TABLE IF NOT EXISTS dog(`
 	id_user INT NOT NULL,
 	PRIMARY KEY(id),
 	FOREIGN KEY(id_user) REFERENCES user(id)
-`)
+`);
 
 CREATE TABLE IF NOT EXISTS location(`
 	id INT NOT NULL AUTO_INCREMENT,
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS location(`
 	description TEXT NOT NULL,
 	image_url TEXT NOT NULL,
 	PRIMARY KEY(id)
-`)
+`);
 
 CREATE TABLE IF NOT EXISTS message(`
 	id INT NOT NULL AUTO_INCREMENT,
@@ -47,7 +47,7 @@ CREATE TABLE IF NOT EXISTS message(`
 	PRIMARY KEY(id),
 	FOREIGN KEY(user_from) REFERENCES user(id),
 	FOREIGN KEY(user_to) REFERENCES user(id)
-`)
+`);
 
 CREATE TABLE IF NOT EXISTS friend_joint(`
 	id_dog INT NOT NULL,
@@ -55,14 +55,14 @@ CREATE TABLE IF NOT EXISTS friend_joint(`
 	bool_friend TINYINT(1) NOT NULL,
 	FOREIGN KEY(id_dog) REFERENCES dog(id),
 	FOREIGN KEY(id_dogFriend) REFERENCES dog(id)
-`)
+`);
 
 CREATE TABLE IF NOT EXISTS fav_location_joint(`
 	id_location INT NOT NULL,
 	id_dog INT NOT NULL,
 	FOREIGN KEY(id_location) REFERENCES location(id),
 	FOREIGN KEY(id_dog) REFERENCES dog(id)
-`)
+`);
 
 /* 
 To use this file run:
