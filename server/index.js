4325c9cae46a8808e98afed3589b171e16afba3a
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-12 17:43:11 -0500

    'added some more middleware and routes'

--- server/index.js
+++ server/index.js
@@ -2,21 +2,50 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const pool = require('./db/db.js');
+const pass = require('./passport/passport');
 // const data = require('../data.json');
 const { getUsers, getDogs, addFriend } = require('./queries.js');
 
-const app = express();
-app.use(express.json());
-app.use(cors());
 const PORT = process.env.PORT || 3000;
 const CLIENT_PATH = path.join(__dirname, '../client/dist');
 
-app.use(express.static(CLIENT_PATH));
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`);
-});
+/* Middleware================================================================== */
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+}
+app.use(express.json());
+app.use(cors());
+app.use(express.static(CLIENT_PATH));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  secure: false,
+}));
+// app.use(isLoggedIn());
+
+/* ============================================================================ */
+
+/* Routes====================================================================== */
+app.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    res.redirect('/');
+  });
 
 app.get('/dogs', (req, res) => {
   getDogs()
@@ -44,4 +73,17 @@ app.post('/friends', (req, res) => {
       console.error(err, 'no friend added.');
       res.sendStatus(500);
     });
+});
+
+app.get('/logout', (req, res) => {
+  req.session = null;
+  req.logout();
+  res.redirect('/login');
+});
+
+/* ============================================================================ */
+
+/* Starting server */
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
 });
