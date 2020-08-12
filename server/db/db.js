89aa75289ee835e9c1139b3cce1d4fbdb77890af
Evan Taft <evtaft22@gmail.com>
2020-08-12 01:08:22 -0500

    "refatored db connection and updated schema"

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,18 @@
-const mariadb = require('mariadb');
+const mysql = require('mysql');
 
-const pool = mariadb.createPool({
+const pool = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
+  password: '',
   database: 'puppy_love',
-  connectionLimit: 5,
 });
 
-pool.getConnection()
-  .then(() => console.log('Connecting to database'))
-  .catch((err) => console.error('Database Error: ', err));
+pool.connect((err) => {
+  if (err) {
+    console.error('Database Error: ', err);
+  } else {
+    console.log('Connecting to database');
+  }
+});
 
 module.exports = pool;
