ebe308d9856803b5f6f2238674c045e1d3840cd3
DennisPintilie <dennis.pintilie.alexandru@gmail.com>
2020-08-13 14:24:39 -0500

    'Deleted logout component and created login component'

--- client/component/Login.jsx
+++ client/component/Login.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 function Login() {
 
   const onConfirm = () => {
-    const restart = confirm('This should redirect to "/" Landing page.');
-    if (restart) {
-      return;
-    }
+    // const restart = confirm('This should redirect to "/" Landing page.');
+    // if (restart) {
+    //   return;
+    // }
+    Axios.get('/auth/google')
+    .then(() => {console.log('Sending to google!')})
+    .catch((err) => console.error(err, 'Not sending to google'));
   };
 
   const onSignup = () => {
@@ -22,7 +26,8 @@ function Login() {
       <h2>Login</h2>
       <input class='input-field' type='text' style={{ width: '500px', height: '35px', fontSize: '16px', margin: '10px 0 10px 340px', paddingLeft: '10px' }}></input>
       <div id='log-butt' style={{marginBottom: '100px'}}>
-        <Link to='/' id='login' onClick={onConfirm}>Login</Link>
+        {/* <Link to='/login' id='login' onClick={onConfirm}>Login</Link> */}
+        <a id='login' href='/auth/google'>Login</a>
         <Link to='/login' id='signup' onClick={onSignup}>Signup</Link>
       </div>
     </div>
