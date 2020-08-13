11211607a0531ae213e4c5e315fdf6799780c460
Evan Taft <evtaft22@gmail.com>
2020-08-12 22:32:18 -0500

    "dogDisplay rendering"

--- client/component/App.jsx
+++ client/component/App.jsx
@@ -36,11 +36,11 @@ function App() {
       .catch((err) => console.error(err, 'Could not get all dogs.'));
    }, []); 
 
-   // useEffect(() => {
-   //    axios.get('/users')
-   //    .then((response) => setUsers(response.data))
-   //    .catch((err) => console.error(err, 'Could not get all users.'));
-   // }, []);
+   useEffect(() => {
+      axios.get('/users')
+      .then((response) => setUsers(response.data))
+      .catch((err) => console.error(err, 'Could not get all users.'));
+   }, []);
 
    const addFriend = (name, friendName, bool) => {
       axios.post('/friends', { name, friendName, bool })
@@ -48,8 +48,7 @@ function App() {
       .catch((err) => console.error(err, 'we couldn\'t add this friend'));
    };
 
-   console.log('choices: ', dogs);
-   // console.log('users: ', users);
+   console.log('users: ', users);
 
    return (
       <Router>
@@ -58,9 +57,9 @@ function App() {
             <button id='settings' onClick={open}>Menu</button>
             <Switch>
                <Route exact={true} path="/" render={() => (<Choice onLike={addFriend} dogs={dogs} />)} />
-               <Route exact path="/login" render={() => (<Login users={users} />)} />
+               {/* <Route exact path="/login" render={() => (<Login users={users} />)} /> */}
                <Route path="/myprofile" render={() => (<MyProfile dogs={dogs} />)} />
-               <Route path="/chats" render={() => (<Chats users={users} />)} />
+               <Route exact path="/chats" render={({ match }) => (<Chats users={users} />)} />
                <Route path="/dogprofile" render={() => (<DogProfile />)} />
                <Route path="/friends" render={() => (<Friends dogs={dogs} />)} />
                <Route path="/favs" render={() => (<FavLocations />)} />
--- client/component/Choice.jsx
+++ client/component/Choice.jsx
@@ -3,43 +3,28 @@ import { Link, Route } from 'react-router-dom';
 
 function Choice({ onLike, dogs }) {
 
-  // const choice = dogs.map(option => {
-  //   return (
-  //     <div id='choice-box'>
-  //       <img src={option.image}/>
-  //       <div id='title'>{option.name}</div>
-  //       <div id='breed'>{option.breed}</div>
-  //       <div id='age'>{option.age}</div>
-  //     </div>
-  //   );
-  // });
-
   const [ index, setIndex ] = useState(0);
   const [ dogDisplay, setDogDisplay ] = useState('');
 
-  // useEffect(() => {
-  //   setDogDisplay(props.dogs[1]);
-  // }, [props.dogs[1]]);
+  useEffect(() => {
+    setDogDisplay(dogs[1]);
+  }, [dogs]);
 
   const dislike = () => {
     setIndex(index + 1);
     if (index < dogs.length) {
       setDogDisplay(() => dogs[index]);
     } else {
-      setDogDisplay(<div id='choice-box'><h4>Looks like you've made it through all the dogs in you're area. Please check back later.</h4></div>);
+      setDogDisplay(<div id='choice-box'><div id='alt'>Looks like you've made it through all the dogs in you're area. Please check back later.</div></div>);
     }
   };
 
-  console.log('dogs: ', dogs);
-  console.log('index: ', index);
-  console.log('dogDisplay: ', dogDisplay);
-
-  // const like = () => {
-  //   onLike('Trigger', dogDisplay.name, true)
-  //   .then(() => dislike())
-  //   .then(() => console.log('this friend was added'))
-  //   .catch((err) => console.error(err, 'we nope'));
-  // };
+  const like = () => {
+    onLike('Trigger', dogDisplay.name, true)
+    .then(() => dislike())
+    .then(() => console.log('this friend was added'))
+    .catch((err) => console.error(err, 'we nope'));
+  };
 
   return (
     <div>
@@ -47,7 +32,7 @@ function Choice({ onLike, dogs }) {
         {dogDisplay}
         <div id='select'>
           <button id='no' onClick={dislike}>No</button>
-          <Link to={'/dogprofile'} id='view'>View Profile</Link>
+          <Link to={`/dogprofile/${dogDisplay.id}`} id='view'>View Profile</Link>
           <button id='yes' onClick={dislike}>Yes</button>
         </div>
       </div>
