6062b506189dcd067a15b9c93a65b504e3354255
HenokSamuel <henoksamuel360@gmail.com>
2020-08-14 09:33:28 -0500

    Friend comp for each dog friend profile is semi functional

--- client/component/Friend.jsx
+++ client/component/Friend.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Friend() {
+function Friend({ id, dog_name, breed, weight, age, fixed, description, image, id_user }) {
   return (
-    <div>
-      <button id='settings' onClick={open}>Menu</button>
-      <h2>Friend</h2>
+    <div class='profileContainer'>
+      <h1>{`${dog_name}'s Profile Information`}</h1>
+      <div class='profileInfo'>{`Name: ${dog_name}`}</div>
+      <div class='profileInfo'>{`Age: ${age}`}</div>
+      <div class='profileInfo'>{`Weight: ${weight}`}</div>
+      <div class='profileInfo'>{`Breed: ${breed}`}</div>
+      <div class='profileInfo'>{`Fixed: ${fixed}`}</div>
+      <div class='profileInfo'>{`Description: ${description}`}</div>
+      <div class='profileInfo'>Photo: <img src={image}></img></div>
     </div>
   );
 };
