5043c1ede8dd4d4c3d49b74a3343226e6dbbb639
Evan Taft <evtaft22@gmail.com>
2020-08-15 11:28:35 -0500

    "authentication and signup built"

--- client/component/Login.jsx
+++ client/component/Login.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 function Login() {
-
-  const onConfirm = () => {
-    const restart = confirm('This should redirect to "/" Landing page.');
-    if (restart) {
-      return;
-    }
-  };
-
-  const onSignup = () => {
-    const signup = confirm('This should show a signup form over the page.');
-    if (singup) {
-      return;
-    }
-  };
-
   return (
     <div>
       <h2>Login</h2>
-      <input class='input-field' type='text' style={{ width: '500px', height: '35px', fontSize: '16px', margin: '10px 0 10px 340px', paddingLeft: '10px' }}></input>
-      <div id='log-butt' style={{marginBottom: '100px'}}>
-        {/* <Link to='/login' id='login' onClick={onConfirm}>Login</Link> */}
-        <a id='login' href='/auth/google'>Login</a>
-        <Link to='/login' id='signup' onClick={onSignup}>Signup</Link>
+      <div id='log-butt' style={{marginBottom: '1000px'}}>
+        <a id='signup' href='/auth/google'>Sign In With Google</a>
       </div>
     </div>
   );
